9cf6fea0e9a3f6d625eb0e436b35d1b518eaf9e5
raghu <raghu.sk@gmail.com>
2017-06-05 22:59:48 +0530

    New work

---
+++ RoboND-Rover-Project/code/solution.py
@@ -0,0 +1,302 @@
+import cv2 # OpenCV for perspective transform
+import numpy as np
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import scipy.misc # For saving images as needed
+import glob  # For reading in a list of images from a folder
+import imageio
+imageio.plugins.ffmpeg.download()
+
+path = '../test_dataset/IMG/*'
+path = '../../../raghu/IMG/*'
+img_list = glob.glob(path)
+# Grab a random image and display it
+idx = np.random.randint(0, len(img_list)-1)
+image = mpimg.imread(img_list[idx])
+plt.imshow(image)
+
+
+example_grid = '../calibration_images/example_grid1.jpg'
+example_grid = '../raghu/IMG/robocam_2017_06_05_22_05_53_987.jpg'
+example_rock = '../calibration_images/example_rock1.jpg'
+example_rock = '../raghu/IMG/robocam_2017_06_05_22_06_06_638.jpg'
+grid_img = mpimg.imread(example_grid)
+rock_img = mpimg.imread(example_rock)
+
+fig = plt.figure(figsize=(12,3))
+plt.subplot(121)
+plt.imshow(grid_img)
+plt.subplot(122)
+plt.imshow(rock_img)
+
+
+def perspect_transform(img, src, dst):
+
+    M = cv2.getPerspectiveTransform(src, dst)
+    warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))# keep same size as input image
+
+    return warped
+
+
+# Define calibration box in source (actual) and destination (desired) coordinates
+# These source and destination points are defined to warp the image
+# to a grid where each 10x10 pixel square represents 1 square meter
+# The destination box will be 2*dst_size on each side
+dst_size = 5
+# Set a bottom offset to account for the fact that the bottom of the image
+# is not the position of the rover but a bit in front of it
+# this is just a rough guess, feel free to change it!
+bottom_offset = 6
+source = np.float32([[14, 140], [301 ,140],[200, 96], [118, 96]])
+destination = np.float32([[image.shape[1]/2 - dst_size, image.shape[0] - bottom_offset],
+                  [image.shape[1]/2 + dst_size, image.shape[0] - bottom_offset],
+                  [image.shape[1]/2 + dst_size, image.shape[0] - 2*dst_size - bottom_offset],
+                  [image.shape[1]/2 - dst_size, image.shape[0] - 2*dst_size - bottom_offset],
+                  ])
+warped = perspect_transform(grid_img, source, destination)
+plt.imshow(warped)
+
+
+def color_thresh(img, rgb_thresh=(160, 160, 160) , rgb_thresh_max=(255, 255, 255)):
+    # Create an array of zeros same xy size as img, but single channel
+    color_select = np.zeros_like(img[:,:,0])
+    # Require that each pixel be above all three threshold values in RGB
+    # above_thresh will now contain a boolean array with "True"
+    # where threshold was met
+    above_thresh = (img[:,:,0] > rgb_thresh[0]) \
+                & (img[:,:,1] > rgb_thresh[1]) \
+                & (img[:,:,2] > rgb_thresh[2])
+
+    below_thresh = (img[:,:,0] < rgb_thresh[0]) \
+                & (img[:,:,1] < rgb_thresh[1]) \
+                & (img[:,:,2] < rgb_thresh[2])
+    # Index the array of zeros with the boolean array and set to 1
+    color_select[above_thresh] = 1
+    #color_select[below_thresh] = 1
+    # Return the binary image
+    return color_select
+
+threshed = color_thresh(warped)
+plt.imshow(threshed, cmap='gray')
+
+
+def rover_coords(binary_img):
+    # Identify nonzero pixels
+    ypos, xpos = binary_img.nonzero()
+    # Calculate pixel positions with reference to the rover position being at the
+    # center bottom of the image.
+    x_pixel = np.absolute(ypos - binary_img.shape[0]).astype(np.float)
+    y_pixel = -(xpos - binary_img.shape[0]).astype(np.float)
+    return x_pixel, y_pixel
+
+# Define a function to convert to radial coords in rover space
+def to_polar_coords(x_pixel, y_pixel):
+    # Convert (x_pixel, y_pixel) to (distance, angle)
+    # in polar coordinates in rover space
+    # Calculate distance to each pixel
+    dist = np.sqrt(x_pixel**2 + y_pixel**2)
+    # Calculate angle away from vertical for each pixel
+    angles = np.arctan2(y_pixel, x_pixel)
+    return dist, angles
+
+# Define a function to apply a rotation to pixel positions
+def rotate_pix(xpix, ypix, yaw):
+    # TODO:
+    # Convert yaw to radians
+    yaw_rad = yaw * np.pi / 180
+    # Apply a rotation
+    xpix_rotated = xpix * np.cos(yaw_rad) - ypix * np.sin(yaw_rad)
+    ypix_rotated = xpix * np.sin(yaw_rad) + ypix * np.cos(yaw_rad)
+    # Return the result
+    return xpix_rotated, ypix_rotated
+
+# Define a function to perform a translation
+def translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale):
+    # TODO:
+    # Apply a scaling and a translation
+    xpix_translated = np.int_(xpos + (xpix_rot / scale))
+    ypix_translated = np.int_(ypos + (ypix_rot / scale))
+    # Return the result
+    return xpix_translated, ypix_translated
+
+# Define a function to apply rotation and translation (and clipping)
+# Once you define the two functions above this function should work
+def pix_to_world(xpix, ypix, xpos, ypos, yaw, world_size, scale):
+    # Apply rotation
+    xpix_rot, ypix_rot = rotate_pix(xpix, ypix, yaw)
+    # Apply translation
+    xpix_tran, ypix_tran = translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale)
+    # Perform rotation, translation and clipping all at once
+    x_pix_world = np.clip(np.int_(xpix_tran), 0, world_size - 1)
+    y_pix_world = np.clip(np.int_(ypix_tran), 0, world_size - 1)
+    # Return the result
+    return x_pix_world, y_pix_world
+
+# Grab another random image
+idx = np.random.randint(0, len(img_list)-1)
+image = mpimg.imread(img_list[idx])
+warped = perspect_transform(image, source, destination)
+threshed = color_thresh(warped)
+
+# Calculate pixel values in rover-centric coords and distance/angle to all pixels
+xpix, ypix = rover_coords(threshed)
+dist, angles = to_polar_coords(xpix, ypix)
+mean_dir = np.mean(angles)
+
+# Do some plotting
+fig = plt.figure(figsize=(12,9))
+plt.subplot(221)
+plt.imshow(image)
+plt.subplot(222)
+plt.imshow(warped)
+plt.subplot(223)
+plt.imshow(threshed, cmap='gray')
+plt.subplot(224)
+plt.plot(xpix, ypix, '.')
+plt.ylim(-160, 160)
+plt.xlim(0, 160)
+arrow_length = 100
+x_arrow = arrow_length * np.cos(mean_dir)
+y_arrow = arrow_length * np.sin(mean_dir)
+plt.arrow(0, 0, x_arrow, y_arrow, color='red', zorder=2, head_width=10, width=2)
+
+# Import pandas and read in csv file as a dataframe
+import pandas as pd
+# Change the path below to your data directory
+# If you are in a locale (e.g., Europe) that uses ',' as the decimal separator
+# change the '.' to ','
+df = pd.read_csv('../test_dataset/robot_log.csv', delimiter=';', decimal='.')
+csv_img_list = df["Path"].tolist() # Create list of image pathnames
+# Read in ground truth map and create a 3-channel image with it
+ground_truth = mpimg.imread('../calibration_images/map_bw.png')
+ground_truth_3d = np.dstack((ground_truth*0, ground_truth*255, ground_truth*0)).astype(np.float)
+
+# Creating a class to be the data container
+# Will read in saved data from csv file and populate this object
+# Worldmap is instantiated as 200 x 200 grids corresponding
+# to a 200m x 200m space (same size as the ground truth map: 200 x 200 pixels)
+# This encompasses the full range of output position values in x and y from the sim
+class Databucket():
+    def __init__(self):
+        self.images = csv_img_list
+        self.xpos = df["X_Position"].values
+        self.ypos = df["Y_Position"].values
+        self.yaw = df["Yaw"].values
+        self.count = -1 # This will be a running index, setting to -1 is a hack
+                        # because moviepy (below) seems to run one extra iteration
+        self.worldmap = np.zeros((200, 200, 3)).astype(np.float)
+        self.ground_truth = ground_truth_3d # Ground truth worldmap
+
+# Instantiate a Databucket().. this will be a global variable/object
+# that you can refer to in the process_image() function below
+data = Databucket()
+print(data.xpos[0], data.ypos[0])
+
+
+def process_image(img):
+    # Example of how to use the Databucket() object defined above
+    # to print the current x, y and yaw values
+    # print(data.xpos[data.count], data.ypos[data.count], data.yaw[data.count])
+
+    # TODO:
+    # 1) Define source and destination points for perspective transform
+    dst_size = 5
+    # Set a bottom offset to account for the fact that the bottom of the image
+    # is not the position of the rover but a bit in front of it
+    # this is just a rough guess, feel free to change it!
+    bottom_offset = 6
+    source = np.float32([[14, 140], [301 ,140],[200, 96], [118, 96]])
+    destination = np.float32([[img.shape[1]/2 - dst_size, img.shape[0] - bottom_offset],
+                  [img.shape[1]/2 + dst_size, img.shape[0] - bottom_offset],
+                  [img.shape[1]/2 + dst_size, img.shape[0] - 2*dst_size - bottom_offset],
+                  [img.shape[1]/2 - dst_size, img.shape[0] - 2*dst_size - bottom_offset],
+                  ])
+    # 2) Apply perspective transform
+    warped = perspect_transform(img, source, destination)
+    #plt.imshow(warped)
+    # 3) Apply color threshold to identify navigable terrain/obstacles/rock samples
+    threshed = color_thresh(warped)
+    wall_threshed = color_thresh(warped, rgb_thresh=(0, 0, 0), rgb_thresh_max=(160, 160, 160))
+    rock_threshed = color_thresh(warped, rgb_thresh=(150, 100, 0), rgb_thresh_max=(255, 200, 50))
+    # 4) Convert thresholded image pixel values to rover-centric coords
+    xpix_nav, ypix_nav = rover_coords(threshed)
+    xpix_wall, ypix_wall = rover_coords(wall_threshed)
+    xpix_rock, ypix_rock = rover_coords(rock_threshed)
+    # 5) Convert rover-centric pixel values to world coords
+    scale = 10
+
+    xpix_world, ypix_world = pix_to_world(xpix_nav, ypix_nav, data.xpos[data.count],
+                                data.ypos[data.count], data.yaw[data.count],
+                                data.worldmap.shape[0], scale)
+
+    xpix_world_wall, ypix_world_wall = pix_to_world(xpix_wall, ypix_wall, data.xpos[data.count],
+                               data.ypos[data.count], data.yaw[data.count],
+                               data.worldmap.shape[0], scale)
+    xpix_world_rock, ypix_world_rock = pix_to_world(xpix_rock, ypix_rock, data.xpos[data.count],
+                               data.ypos[data.count], data.yaw[data.count],
+                               data.worldmap.shape[0], scale)
+    # 6) Update worldmap (to be displayed on right side of screen)
+        # Example: data.worldmap[obstacle_y_world, obstacle_x_world, 0] += 1
+        #          data.worldmap[rock_y_world, rock_x_world, 1] += 1
+        #          data.worldmap[navigable_y_world, navigable_x_world, 2] += 1
+    #data.worldmap[ypix_world, xpix_world, 2] += 1
+    # 7) Make a mosaic image, below is some example code
+        # First create a blank image (can be whatever shape you like)
+
+
+    output_image = np.zeros((img.shape[0] + data.worldmap.shape[0], img.shape[1]*2, 3))
+        # Next you can populate regions of the image with various output
+        # Here I'm putting the original image in the upper left hand corner
+    output_image[0:img.shape[0], 0:img.shape[1]] = img
+
+        # Let's create more images to add to the mosaic, first a warped image
+    warped = perspect_transform(img, source, destination)
+        # Add the warped image in the upper right hand corner
+    output_image[0:img.shape[0], img.shape[1]:] = warped
+
+        # Overlay worldmap with ground truth map
+    #map_add = cv2.addWeighted(data.worldmap, 1, data.ground_truth, 0.5, 0)
+        # Flip map overlay so y-axis points upward and add to output_image
+    #output_image[img.shape[0]:, 0:data.worldmap.shape[1]] = np.flipud(map_add)
+
+
+    map_add = cv2.addWeighted(data.worldmap, 1, data.ground_truth, 0.5, 0)
+        # Flip map overlay so y-axis points upward and add to output_image
+    output_image[img.shape[0]:, 0:data.worldmap.shape[1]] = map_add
+
+        # Overlay worldmap with navigable terrain points in blue
+    output_image[img.shape[0]:, 0:data.worldmap.shape[1], 2][ypix_world, xpix_world] += 255
+        # Overlay worldmap with wall points in red
+    output_image[img.shape[0]:, 0:data.worldmap.shape[1], 0][ypix_world_wall, xpix_world_wall] += 255
+        # Overlay worldmap with rock points in green
+    output_image[img.shape[0]:, 0:data.worldmap.shape[1], 1][ypix_world_rock, xpix_world_rock] += 255
+
+
+        # Then putting some text over the image
+    cv2.putText(output_image,"Populate this image with your analyses to make a video!", (20, 20),
+                cv2.FONT_HERSHEY_COMPLEX, 0.4, (255, 255, 255), 1)
+    data.count += 1 # Keep track of the index in the Databucket()
+
+    return output_image
+
+
+
+from moviepy.editor import VideoFileClip
+from moviepy.editor import ImageSequenceClip
+
+
+# Define pathname to save the output video
+output = '../output/test_mapping.mp4'
+data = Databucket() # Re-initialize data in case you're running this cell multiple times
+clip = ImageSequenceClip(data.images, fps=60) # Note: output video will be sped up because
+                                          # recording rate in simulator is fps=25
+new_clip = clip.fl_image(process_image) #NOTE: this function expects color images!!
+new_clip.write_videofile(output, audio=False)
+
+#import io
+#import base64
+#video = io.open(clip, 'r+b').read()
+#encoded_video = base64.b64encode(video)
+#HTML(data='''<video alt="test" controls>
+#                <source src="data:video/mp4;base64,{0}" type="video/mp4" />
+#             </video>'''.format(encoded_video.decode('ascii')))
